87c789261da6db050cb57567c5c9094b870c1ab0
10712073 <10712073@LTILMAA21200686>
2023-10-18 12:08:11 +0530

    CodeFix has done

--- z_project/Palindrome.py
+++ z_project/Palindrome.py
@@ -1,13 +1,11 @@
-#This program checks for the palindrome
-
-def palindrome(strg)
+def palindrome(string):
     '''This function checks the string for palindrome'''
-    revS = string[::-1]
+    revString = string[::-1]
     if string == revString:
         print('String is Palindrome')
     else:
         print('String is not Palindrome')
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     userInput = str(input('Enter a string to check for Palindrome: '))
-    palindrome(useput)
+    palindrome(userInput)
