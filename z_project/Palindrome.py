9e18e803c7b2242c7ec3689a655efcdf3d32f814
AishwaryaaRaghuraman <143588049+AishwaryaaRaghuraman@users.noreply.github.com>
2023-10-18 12:10:30 +0530

    Update Palindrome.py

--- z_project/Palindrome.py
+++ z_project/Palindrome.py
@@ -1,11 +1,11 @@
-def palindrome(string):
+def palindrome(sing)
     '''This function checks the string for palindrome'''
     revString = string[::-1]
-    if string == revString:
+    if string == revString
         print('String is Palindrome')
     else:
         print('String is not Palindrome')
 
-if __name__ == '__main__':
+if __name__ == '__
     userInput = str(input('Enter a string to check for Palindrome: '))
-    palindrome(userInput)
+    palindrome(userput)
