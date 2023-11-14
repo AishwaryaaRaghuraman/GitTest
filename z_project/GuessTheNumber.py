9381223aafbbbd6899a044dfb60306ab426476ca
10712073 <10712073@GMW3S93>
2023-11-14 12:38:17 +0530

    pavan changed

--- z_project/GuessTheNumber.py
+++ z_project/GuessTheNumber.py
@@ -1,12 +1,14 @@
 
+
+Answer:
 import random
 
-def guess(
+def guess():
     ''' This function guesses the randomly generated number '''
     randomNumber = random.randint(0, 21)
     cnt = 0 
 
-    while True
+    while True:
         cnt += 1 
         number = int(input('Enter the number between 0 to 20: '))
         if number < randomNumber:
@@ -17,5 +19,5 @@ def guess(
             print('You have got it in', cnt, 'tries')
             break
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     guess()
