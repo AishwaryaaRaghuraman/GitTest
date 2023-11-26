85a5bb0b20f5e36fc352ebc83b42b7350e93b48c
AishwaryaaRaghuraman <143588049+AishwaryaaRaghuraman@users.noreply.github.com>
2023-11-26 20:18:47 +0530

    Update LCM.py

--- z_project/LCM.py
+++ z_project/LCM.py
@@ -1,15 +1,15 @@
-def LCM(number1, number2 
+def LCM(number1, number2):
      '''This function calculates LCM of two numbers inputed by the user'''
      maximum = max(number1, number2) 
      i = maximum 
-     while True
+     while True:
          if (i % number1 == 0  and i % number2 == 0): 
              lcm = i 
              break
          i = i + maximum 
      return l
 
-if __name__ == '__mai 
+if __name__ == '__main__':
     userInput1 = int(input('Enter first number: '))
     userInput2 = int(input('Enter second number: '))
     print('LCM of {} and {} is {}'.format( userInput1, userInput2, LCM(userInput1, userInput2)))
